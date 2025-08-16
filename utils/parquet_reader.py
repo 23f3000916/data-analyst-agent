22730393951a1ced5acfd6984aec577884acd161
23f3000916 <23f3000916@ds.study.iitm.ac.in>
2025-08-16 12:19:34 +0530

    .

--- utils/parquet_reader.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-
-def run_duckdb_analysis(s3_path: str, query: str):
-    duckdb.sql("INSTALL httpfs; LOAD httpfs")
-    duckdb.sql("INSTALL parquet; LOAD parquet")
-    print (f"Running query on {s3_path} with DuckDB")
-    return duckdb.sql(query).df()
